4a2248463b80af841f3c6d8fc1a50b7823e44400
Kendra Staver <staverkendra@gmail.com>
2021-10-08 16:33:41 -0700

    Basic Structures and files setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const App = require("./lib/App");
+const inquirer = require("inquirer");
+
+// Initialize a new Team Profile object
+const app = new App();
+
+// Start Team Profile Object
+app.start();
