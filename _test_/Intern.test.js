0bb0c1493bdb855a0e6fca908cf8768ce5b59a1b
Kendra Staver <staverkendra@gmail.com>
2021-10-11 00:01:33 -0700

    qoutes were missing on line 11

--- _test_/Intern.test.js
+++ _test_/Intern.test.js
@@ -8,7 +8,7 @@ test("Can set school via constructor", () => {
 
 test("getRole() should return \"Intern\"", () => {
     const testValue = "Intern";
-    const e = new Intern("Foo", 1, "test@test.com", UC Berkeley);
+    const e = new Intern("Foo", 1, "test@test.com", "UC Berkeley");
     expect(e.getRole()).toBe(testValue);
 });
 
