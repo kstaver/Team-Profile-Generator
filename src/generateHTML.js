95153e863417760d55884c3fce827a23ddd8e617
Kendra Staver <staverkendra@gmail.com>
2021-10-13 19:04:36 -0700

    i forgot that .css was in the same folder as index

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -106,7 +106,7 @@ const generateTeamPage = function(employeeCards){
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" />
 
         <!--Kendra's Stylesheet-->
-        <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+        <link rel="stylesheet" type="text/css" href="./style.css">
     </head>
     <body>
         <header>
